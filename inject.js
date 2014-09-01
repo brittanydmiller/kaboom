dfc0c387b21dbba211335936c60a8f006d2d1c56
Brittany Miller <brittanydmiller@gmail.com>
2014-09-01 11:59:24 -0700

    Adds ability to filter based on self-written title and current job title.

--- inject.js
+++ inject.js
@@ -7,6 +7,71 @@ var injected = injected || (function(){
   // we can use from our event script.
   var methods = {};
 
+  methods.contains = function(array, target) {
+    var j = array.length;
+    while (j--) {
+      if (array[j] === target) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  methods.parseCurrentTitle = function(vCard){
+    var titleWords = "" ;
+    if (vCard[1].children[1].className == "title") {
+      var titleWords = vCard[1].children[1].innerText;
+    }
+    var currentJobWords = vCard[3].children[1].children[0].innerText;
+    var isolatedWords = methods.toJustWords(titleWords).concat(methods.toJustWords(currentJobWords));
+    isolatedWords = methods.eliminateDupes(isolatedWords);
+    return isolatedWords;
+  };
+
+  methods.toJustWords = function(data) {
+    var words = data.toLowerCase()
+      .replace(/['!"#$%&\\'()\*+,\-\.\/:;<=>?@\[\\\]\^_`{|}~']/g,"")
+      .split(" ");
+    words = methods.removeCompany(words, "at");
+    return words;
+  };
+
+  methods.eliminateDupes = function(arr) {
+    var i,
+        len=arr.length,
+        out=[],
+        obj={};
+
+    for (i=0;i<len;i++) {
+      obj[arr[i]]=0;
+    }
+    for (i in obj) {
+      out.push(i);
+    }
+    return out;
+  };
+
+  methods.removeCompany = function(wordsArray, target){
+    if (methods.contains(wordsArray, "at")) {
+      //chops off second line of current postion data, as customer requested.
+      var sliceTarget = wordsArray.indexOf("at");
+      return wordsArray.slice(0, sliceTarget);
+    } else {
+    return wordsArray;
+    }
+  };
+
+  methods.lowQuality = function(jobTitle){
+    var badTitles = ["intern", "contractor", "contract", "consultant", "consulting", "self-employed", "self employed", "junior", "jr", "jr.", "freelance", "freelancer", "student", "founder", "CTO", "CEO", "co-founder", "ta"]
+    //var badCompanies = ["self employed", "freelance", "self-employed"];
+    for ( k in badTitles ) {
+      if ( methods.contains(jobTitle, badTitles[k]) ) {
+        return true;
+      }  
+    }
+    return false;
+  };
+
   methods.filterCandidates = function(){
     var urls = [];
     var profiles = document.querySelectorAll("li.results-item.profile");
@@ -14,11 +79,13 @@ var injected = injected || (function(){
     for (var i = 0; i < profiles.length; i ++) {
       var vCard = profiles[i].children[1].children[2].children;
       var viewedAlready = vCard[2].children[0];
+      var jobTitle = methods.parseCurrentTitle(vCard);
       var profileLink = vCard[0].children[0].getAttribute("href")
       
       if (viewedAlready) {
         profiles[i].style.display = "none";
-        console.log("seen already");
+      } else if ( methods.lowQuality(jobTitle) ) {
+        profiles[i].style.display = "none";
       } else { 
         urls.push(profileLink);       
       }
